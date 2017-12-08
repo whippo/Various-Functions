27b795fa2c1af8887ac4c0ecd1ad4d4736c60c7a
whippo <rosspitality@gmail.com>
2017-12-08 14:23:54 -0500

    fixed offset notation with if() condition

--- function_printUTCoffset.R
+++ function_printUTCoffset.R
@@ -9,7 +9,10 @@ current_utc_offset <- function () {
     now_timestamp = as.character(Sys.time()),
     utc_timestamp = as.character(as_datetime(Sys.time(), tz = "UTC")))
   time_frame$hours = with(time_frame, difftime(now_timestamp, utc_timestamp, units = "hours"))
-  time_frame$hours <- as.numeric(time_frame$hours*1000)
+  time_frame$hours <- as.character(as.numeric(time_frame$hours*100))
+  time_frame$hours <- if (nchar(time_frame$hours) == 4) {
+    gsub("-", "-0", time_frame$hours)
+  }
   current <- unite(time_frame, print_time, c("now_timestamp", "hours"), sep = "")
   return(current$print_time)
 }
